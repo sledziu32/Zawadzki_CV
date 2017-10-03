7d198980101f200cffc8fe6d701620296cb348ce
Przemysław Zawadzki <sledziu32@gmail.com>
2017-10-03 19:15:36 +0200

    utworzenie repozytorium z aplikacja

---
+++ Zawadzki_CV.py
@@ -0,0 +1,19 @@
+u"""Unicode
+Dziękuję za zainteresowanie. Poniższa aplikacja jest moim skromnym CV.
+W momencie zmiany ścieżki zawodowej nie mogę pochwalić się doświadczeniem określonym w czasie.
+
+"""
+
+from tkinter import *
+
+""" utworzenie okienka, nadanie tytułu i rozmiaru """
+root = Tk()
+root.title('Zawadzki CV')
+mainFrame = Frame(root, width=600, height=400)
+mainFrame.grid()
+
+
+
+
+
+root.mainloop()
